52f41ed1098c4f46a56a191b525f15440a496b96
Songhee Han <83492367+song-hee-1@users.noreply.github.com>
2023-06-17 20:44:35 +0900

    Update update_blog_post.py

--- update_blog_post.py
+++ update_blog_post.py
@@ -15,33 +15,27 @@ for idx, entrie in enumerate(rss_feed['entries']):
 
 preREADME = """
 
-</br>
+#### 🙇🏻‍♀️ Introduce. 
 
-### 🙇🏻‍♀️ Introduce.
-프로그래밍이란 적절한 도구를 이용하여 문제를 해결 할 수 있는 능력이라고 생각합니다.
-하나의 기능을 만들 수 있는 여러 가지 방법에 대해 고민하고 탐구하는 편입니다.
+- 프로그래밍이란 적절한 도구를 이용하여 문제를 해결 할 수 있는 능력이라고 생각합니다.
+- 하나의 기능을 만들 수 있는 여러 가지 방법에 대해 고민하고 탐구하는 편입니다.  
 
-</br>
+#### 🎓 Education.
+Chemical Engineering at Hongik University.  
 
-### 🎓 Education.
-Chemical Engineering at Hongik University.
+ 
+#### 📫 Contact.
+song.heee.1@gmail.com. 
 
-</br>
 
-### 📫 Contact.
-song.heee.1@gmail.com
+#### ✍️ Tech Blog.
+https://medium.com/@heeee. 
 
-</br>
 
-✍️ Tech Blog.
-https://medium.com/@heeee
+#### ✨ Interest.
+Python, Django, Swift, PostgreSQL, Azure, Vim. 
 
-</br>
-
-✨ Interest.
-Python, Django, Swift, PostgreSQL, Azure, Vim
-
-</br>
+#### ✏️ Recent blog posts.  
 """
 
 resultREADME = f"{preREADME}{latest_posts}"
